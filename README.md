f1d83b2c3e96eaa3a694cc57742eda35e5b227a5
ronald quichimbo <ronal1923@gmail.com>
2023-07-11 09:37:27 -0500

    services product- secrets v2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ***Devsecops Disign.***
 
 Este repositorio esta con fines educactivos, la cual se lleva a cabo el diseño e implementación de un enfoque de seguridad Devsecops basado en aplicaciones de microservicios.
-
+<img alt="gitleaks badge" src="https://img.shields.io/badge/protected%20by-gitleaks-blue">
 Realizado por developer Ronald 
--- product/services/db.py
+++ product/services/db.py
@@ -3,8 +3,10 @@ from pymongo.server_api import ServerApi
 
 
 DB_NAME = 'Product'
-password='devsecops2023'
-uri = "mongodb+srv://tesis:{password}@tesisdevsecops2023.wdmpqj0.mongodb.net/?retryWrites=true&w=majority"
+user='tesis'
+password = 'devsecops2023'
+
+uri = "mongodb+srv://{user}:{password}@tesisdevsecops2023.wdmpqj0.mongodb.net/?retryWrites=true&w=majority"
 
 connection = MongoClient(uri, server_api=ServerApi('1'))
 db_client = connection[DB_NAME]
