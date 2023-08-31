0092eb72631fa6912a7d9a580c90362800d2b8bc
ronald quichimbo <ronal1923@gmail.com>
2023-08-31 17:01:08 -0500

    deploy app runner

---
+++ product/services/db.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+DB_NAME = 'Product'
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+if username is None or password is None:
+    print("Error credenciales")
+else:
+    uri = ("mongodb+srv://" + username + ":" + password +
+           "@tesisdevsecops2023.wdmpqj0.mongodb.net/" +
+           "?retryWrites=true&w=majority")
+
+
+connection: MongoClient = MongoClient(uri, server_api=ServerApi('1'))
+db_client = connection[DB_NAME]
