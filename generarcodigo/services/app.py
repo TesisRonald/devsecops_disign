1540565e85670f2872f9ce4df98fb42fb98917ca
ronald quichimbo <ronal1923@gmail.com>
2023-09-06 17:42:32 -0500

    microservicio-generar-codigo-feature:correcion flake 8

--- generarcodigo/services/app.py
+++ generarcodigo/services/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 import uuid
 
+
 app = Flask(__name__)
 app.debug = True
 """
 Este metodo sirve para generar el serial.
 """
+
 
 @app.route("/")
 def code():
@@ -13,13 +15,16 @@ def code():
     result = {"codigo": serial}
     return jsonify(result)
 
+
 def generat_serial():
     """
     Este código sirve para generar un código único
     """
     # Genera un UUID y toma los primeros 10 caracteres
+
     serial = str(uuid.uuid4())[:10]
     return serial
+
 
 if __name__ == '__main__':
     app.run()
