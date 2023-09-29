491c23f5741280d476b1e1e64d0d1d8b0d9cf8d0
ronald quichimbo <ronal1923@gmail.com>
2023-09-29 08:54:25 -0500

    microservicio-notificacion-feature: correcion

--- notifications/services/app.py
+++ notifications/services/app.py
@@ -1,19 +1,21 @@
-"""This module contains code for a notification microservice in Flask."""
+"""Microservcio Notificacion """
 import requests
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/notifications')
 def send_notifications():
-    # Make a request to the /products/expiring route to get the list of expiring products
+    #obtienes los productos a caducar 
     response = requests.get('127.0.0.1:5000/products/expiring')
     expiring_products = response.json()
 
-    # Make another request to a different route to get the names of the products
+    # Obtienes los productos por nombre
     product_names = []
     for product in expiring_products:
-        response = requests.get(f'127.0.0.1:5000/products/{product["id"]}/name')
+        response = requests.get(f'127.0.0.1:5000/products/{product["id"]}/'
+                    'name')
         product_name = response.json()
         product_names.append(product_name)
 
