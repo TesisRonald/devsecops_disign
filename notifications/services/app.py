7cd4c118916db7abdbd17907b0a19d95dee07b2b
ronald quichimbo <ronal1923@gmail.com>
2023-10-02 11:42:20 -0500

    microservicio-notificacion-feature: correcion

--- notifications/services/app.py
+++ notifications/services/app.py
@@ -8,17 +8,22 @@ app = Flask(__name__)
 @app.route('/notifications')
 def send_notifications():
     # obtienes los productos a caducar
-    response = requests.get('127.0.0.1:5000/products/expiring')
+    response = requests.get(
+        'https://fyqn8yqxea.us-west-2'
+        '.awsapprunner.com/inventario/caduca'
+    )
     expiring_products = response.json()
 
     # Obtienes los productos por nombre
     product_names = []
     for product in expiring_products:
-        response = requests.get(f'127.0.0.1:5000/products/{product["id"]}/'
-                                'name')
+        url= f'https://s93n3ben38.us-west-2.awsapprunner'\
+            f'.com/products/{product["idproducto"]}'
+        response = requests.get(url)
         product_name = response.json()
         product_names.append(product_name)
 
+    
     # Send notifications for each expiring product
     for product_name in product_names:
         # Code to send notification for each product_name
